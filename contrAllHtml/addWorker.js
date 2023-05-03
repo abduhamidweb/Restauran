e6dff8ce027b067b7878bef54267cc7958d5ae0c
Abduhamid <abduhamidbotirovweb@gmail.com>
2023-05-03 22:15:21 +0500

    admin add worker

--- contrAllHtml/addWorker.js
+++ contrAllHtml/addWorker.js
@@ -7,23 +7,25 @@ let resId = localStorage.getItem("adminres_id");
 addadmin.addEventListener("submit", async (e) => {
     e.preventDefault();
     try {
-          const formData = new FormData();
-          formData('username', workername.value)
-          formData('email', workeremail.value)
-          formData('password', Password2.value)
-          formData('userPhone', workerPhone.value)
-          formData('userInfo', workerinfo.value)
-          formData('workingTime', workerTime.value)
-          formData('userPhoto', workingImg.files[0])
-          formData('salary', workersalary.value)
-          formData('rol', workerRol.value)
-          formData('res_id', workersres.value)
+        const formData = new FormData();
+        formData.append('username', workername.value)
+        formData.append('email', workeremail.value)
+        formData.append('password', Password2.value)
+        formData.append('userPhone', workerPhone.value)
+        formData.append('userInfo', workerinfo.value)
+        formData.append('workingTime', workerTime.value)
+        formData.append('file', workerImg.files[0])
+        console.log('workerImg :', workerImg);
+        formData.append('salary', workersalary.value)
+        formData.append('rol', workerRol.value)
+        formData.append('res_id', workersres.value)
         await fetch('http://localhost:5000/api/worker', {
             method: 'POST',
             headers: {
-                'Content-Type': 'application/json'
+                'enctype': 'multipart/form-data'
             },
             body: formData
+
         });
 
     } catch (error) {
