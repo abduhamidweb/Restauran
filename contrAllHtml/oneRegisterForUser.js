1ae285cb6928f27d835d7bdafbb7625f9731aaf1
Abduhamid <abduhamidbotirovweb@gmail.com>
2023-05-05 21:14:47 +0500

    GET static data

--- contrAllHtml/oneRegisterForUser.js
+++ contrAllHtml/oneRegisterForUser.js
@@ -2,3 +2,32 @@
     let tokenbos = localStorage.getItem("rest-id");
     if (!tokenbos) return location = "AllRestForUser.html";
 })();
+
+let BASE_URL = 'http://localhost:5000/api/';
+(async () => {
+    let response = await fetch(BASE_URL + "restaurants/" + localStorage.getItem("rest-id"));
+    let GET = await response.json();
+    bcgvhdjdwvg2.innerHTML = GET.contact
+    const today = new Date();
+    const daysOfWeek = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+    const dayOfWeek = daysOfWeek[today.getDay()];
+    if (dayOfWeek !== "Sun" && dayOfWeek !== "Mon") {
+        const startTime = new Date();
+        startTime.setDate(startTime.getDate() + (8 + (7 - startTime.getDay())) % 7);
+        startTime.setHours(11);
+        startTime.setMinutes(0);
+
+        const endTime = new Date();
+        endTime.setDate(endTime.getDate() + (8 + (7 - endTime.getDay())) % 7);
+        endTime.setHours(23);
+        endTime.setMinutes(0);
+        sabdhjbhds.innerHTML = ` ${dayOfWeek}-${daysOfWeek[(daysOfWeek.indexOf(dayOfWeek) + 5) % 7]}: ${startTime.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit', hour12: true })} - ${endTime.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit', hour12: true })}`
+
+    } else {
+         sabdhjbhds.innerHTML = 'Closed'
+    }
+
+
+
+
+})()
