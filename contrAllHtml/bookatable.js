5979abebb03b914574c35814de90d49f491a6e0d
Abduhamid <abduhamidbotirovweb@gmail.com>
2023-05-08 15:31:45 +0500

    bookatable end html and css

---
+++ contrAllHtml/bookatable.js
@@ -0,0 +1,6 @@
+let tokenbos = localStorage.getItem("tokenadmin");
+let resId = localStorage.getItem("adminres_id");
+(() => {
+    if (!tokenbos) return location = "adminLogin.html";
+    if (!resId) return location = "adminLogin.html";
+})();
