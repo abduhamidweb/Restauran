09608e9c5c6a2d79cfc4fcfef5eef79481030ac5
Abduhamid <abduhamidbotirovweb@gmail.com>
2023-05-14 19:46:16 +0500

    test to'liq yakunlandi.

--- contrAllHtml/bossAddAdmin.js
+++ contrAllHtml/bossAddAdmin.js
@@ -19,7 +19,6 @@ addadmin.addEventListener("submit", async (e) => {
             })
         });
         let result = await data.json()
-        console.log('result :', result);
         result ? location.reload() : null
 
     } catch (error) {
